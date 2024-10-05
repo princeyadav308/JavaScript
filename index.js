e66d3b69012363ae284080bea505890d2cd697b2
Prince Yadav <85250936+princeyadav308@users.noreply.github.com>
2024-10-05 15:25:19 +0000

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 "use strict"; //treat all JS Code as newer version
+
+let name = "captain"
+
+let age = 21
+
+let isLoggedIn = false
+
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+console.log(typeof null)
+console.table([name, age, isLoggedIn, state, null, undefined]);
