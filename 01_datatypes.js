2d5aa14f5390860f7b5403b0dea24636a0d19ef9
Prince Yadav <85250936+princeyadav308@users.noreply.github.com>
2024-10-05 16:05:44 +0000

    third commit

--- 01_datatypes.js
+++ 01_datatypes.js
@@ -1,11 +1,11 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -16,9 +16,29 @@ let isLoggedIn = "captain"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
 // "captain" => true
 
+// operations //
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+
+let gameCounter = 100
+
+gameCounter++ //prefix and postfix in JS
+
+console.log(gameCounter);
