ad6524946bb5c099574ae73f4a90fc45785874d0
Prince Yadav <85250936+princeyadav308@users.noreply.github.com>
2024-10-07 16:40:35 +0000

    eigth commit

---
+++ 04_number.js
@@ -0,0 +1,44 @@
+const score = 100
+// console.log(score);
+
+
+const balance = new Number(10)
+// console.log(balance);
+
+
+// console.log(balance.toString().length );
+// console.log(balance.toFixed(2) );
+
+
+const otherNumber = 23.73434
+
+// console.log(otherNumber.toPrecision(2));
+
+
+
+const hundred = 100000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+// ========= maths ============
+
+// console.log(Math.PI.toPrecision(5));
+
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*100 +1));
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+ min);
+
+
+
+
+
+
+
+
+
