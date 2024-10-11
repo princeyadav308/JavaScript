138908aabf8123a2b0efc3cfb08459c57399ec6d
Prince Yadav <85250936+princeyadav308@users.noreply.github.com>
2024-10-11 16:25:13 +0000

    eleventh commit

--- Basics_2/07_objects.js
+++ Basics_2/07_objects.js
@@ -24,12 +24,131 @@ JsUser.email="captainbruce@gmail.com"
 // Object.freeze(JsUser) 
 
 JsUser.greeting = function() {
-    console.log("Hello JS User");
+    // console.log("Hello JS User");
 }
 JsUser.greeting2 = function() {
-    console.log(`Hello JS User,${this.email}`);
+    // console.log(`Hello JS User,${this.email}`);
 }
 
-console.log(JsUser.greeting);
-console.log(JsUser.greeting());
-console.log(JsUser.greeting2());
+// console.log(JsUser.greeting);
+// console.log(JsUser.greeting());
+// console.log(JsUser.greeting2());
+
+
+
+
+//////////////////
+
+// const instaUser = new Object()
+const instaUser = {}
+instaUser.id = "123abc"
+instaUser.name = "Captain"
+instaUser.isLoggedIn = false
+
+// console.log(instaUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "Captain",
+            lastname: "Bruce"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "a",
+    4: "b"
+}
+
+// const obj3 = {obj1,obj2}
+
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+
+// console.log(obj3);
+
+
+
+const users = [
+   {
+    userid:"234",
+    email : "email@gmali.com"
+   },
+   {
+    userid:"123",
+    email : "mailemail@gmali.com"
+   },
+   {
+    userid:"345",
+    email : "mail@gmali.com"
+   }
+]
+
+
+users[1].email
+// console.log(instaUser);
+
+// console.log(Object.keys(instaUser));
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));
+
+// console.log(instaUser.hasOwnProperty('isLogged'));
+
+
+
+/////////////////////// object api
+
+const course = {
+    courseName: "js course",
+    price: "345",
+    courseInstructor: "captian"
+}
+
+const {courseInstructor: inst} = course
+
+console.log(inst);
+
+
+// api data is in json format
+
+// {
+//     "name" : "captian",
+//     "courseName":"js in hindi",
+//     "price":"234345"
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
