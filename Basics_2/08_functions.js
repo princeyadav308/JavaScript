18261f476e1821f75ccd771304f5192c0690f1c8
Prince Yadav <85250936+princeyadav308@users.noreply.github.com>
2024-10-13 15:57:02 +0000

    twelvth commit

---
+++ Basics_2/08_functions.js
@@ -0,0 +1,105 @@
+function sayMyName(){
+    console.log("C");
+    console.log("A");
+    console.log("P");
+    console.log("T");
+    console.log("A");
+    console.log("I");
+    console.log("N");
+}
+
+// sayMyName()
+// sayMyName is refrence and () is execution
+// (number1,number2) is known as parameter
+
+function addTwonumber (number1,number2){
+    console.log(number1+ number2);
+}
+
+
+// addTwonumber(2,3)
+//(2,3) is known as arguments
+
+// const result = addTwonumber(2,3)
+// console.log("Result" , result);
+
+// another method
+
+
+
+function addTwonumber (number1,number2){
+    // let result = number1 + number2
+    // return result
+
+    //another method
+
+    return number1 + number2
+}
+
+const result = addTwonumber(2,3)
+
+// console.log("Result is" , result);
+
+
+
+function isLoggedIn(username = "CAPTAIN"){
+    // if(username === undefined){
+    //     console.log("Please enter username");
+    //     return
+    // }
+    if(!undefined){
+        console.log("Please enter username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(isLoggedIn("Captain"))
+console.log(isLoggedIn("CAPTAIN"))
+
+
+
+
+function calculateCartPrice(val1,val2,...num1) { // ... is res operator 
+    return num1
+}
+
+calculateCartPrice(2)
+
+// console.log(calculateCartPrice(20,23,435,23));
+
+
+const user = {
+    username: "Captain",
+    price: 234
+}
+
+function handleObject(anyObject) {
+    // console.log(`Username is ${anyObject.username} price is ${anyObject.price}`);
+    
+}
+
+// handleObject(user)
+
+
+//another method
+
+handleObject({
+    username : "Captain",
+    price: 345
+})
+
+// handleObject(user)
+
+
+// another method
+
+const myNewArr = [234,3245,456,234]
+
+function returnSecondValue(getArray) {
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArr));
+
+
